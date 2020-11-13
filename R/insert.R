ca77a438609ade1d2cf3808ac557e1cc4b530bb3
Painter <957028646czx@gmail.com>
2020-11-13 21:18:01 +0800

    修正了 insertText 的问题

--- R/insert.R
+++ R/insert.R
@@ -1,79 +1,79 @@
 #' Insert \%in\%.
 #' @export
 a1 <- function() {
-  rstudioapi::insertText(" %in% ")
+  rstudioapi::insertText(Inf, " %in% ")
 }
 
 #' Insert \%>\%.
 #' @export
 a2 <- function() {
-  rstudioapi::insertText(" %>% ")
+  rstudioapi::insertText(Inf, " %>% ")
 }
 
 #' Insert \%T>\%.
 #' @export
 a3 <- function() {
-  rstudioapi::insertText(" %T>% ")
+  rstudioapi::insertText(Inf, " %T>% ")
 }
 
 #' Insert <-.
 #' @export
 a4 <- function() {
-  rstudioapi::insertText(" <- ")
+  rstudioapi::insertText(Inf, " <- ")
 }
 
 
 #' Insert \%==\% .
 #' @export
 a5 <- function() {
-  rstudioapi::insertText(" %==% ")
+  rstudioapi::insertText(Inf, " %==% ")
 }
 
 #' Insert \%$\% .
 #' @export
 a6 <- function() {
-  rstudioapi::insertText(" %$% ")
+  rstudioapi::insertText(Inf, " %$% ")
 }
 
 #' Insert \%\% .
 #' @export
 a7 <- function() {
-  rstudioapi::insertText(" %% ")
+  rstudioapi::insertText(Inf, " %% ")
 }
 
 #' Insert \%\/\% .
 #' @export
 a8 <- function() {
-  rstudioapi::insertText(" %/% ")
+  rstudioapi::insertText(Inf, " %/% ")
 }
 
 #' Insert -> .
 #' @export
 a9 <- function() {
-  rstudioapi::insertText(" -> ")
+  rstudioapi::insertText(Inf, " -> ")
 }
 
 
 #' Insert \%+replace\% .
 #' @export
 a10 <- function() {
-  rstudioapi::insertText(" %+replace% ")
+  rstudioapi::insertText(Inf, " %+replace% ")
 }
 
 #' Insert \%w/o\% .
 #' @export
 a11 <- function() {
-  rstudioapi::insertText(" %w/o% ")
+  rstudioapi::insertText(Inf, " %w/o% ")
 }
 
 #' Insert \%*\% .
 #' @export
 a12 <- function() {
-  rstudioapi::insertText(" %*% ")
+  rstudioapi::insertText(Inf, " %*% ")
 }
 
 #' Insert \%<>\% .
 #' @export
 a13 <- function() {
-  rstudioapi::insertText(" %<>% ")
+  rstudioapi::insertText(Inf, " %<>% ")
 }
